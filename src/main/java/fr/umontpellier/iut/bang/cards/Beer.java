b4062aa31a5b92553bfd4c3bf14199d4c2e7a10d
BastienGavioli <bastien.gavioli34@gmail.com>
2021-04-14 23:20:28 +0200

    Ajout de la fonction heal() dans la classe Beer

--- src/main/java/fr/umontpellier/iut/bang/cards/Beer.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Beer.java
@@ -1,9 +1,17 @@
 package fr.umontpellier.iut.bang.cards;
 
+import fr.umontpellier.iut.bang.Player;
+
 public class Beer extends OrangeCard {
 
     public Beer(int value, CardSuit suit) {
         super("Beer", value, suit);
     }
+
+    public void heal(Player player) {
+        player.incrementHealth(1);
+    }
+
+
 
 }
