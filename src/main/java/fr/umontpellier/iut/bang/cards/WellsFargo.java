02f8721bc488489d48bc343d65fd9ef748c00400
Mélanie Fressard <fressardmelanie@gmail.com>
2021-04-23 14:16:00 +0200

    wells fargot

--- src/main/java/fr/umontpellier/iut/bang/cards/WellsFargo.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/WellsFargo.java
@@ -1,8 +1,15 @@
 package fr.umontpellier.iut.bang.cards;
 
+import fr.umontpellier.iut.bang.Player;
+
 public class WellsFargo extends OrangeCard {
     public WellsFargo(int value, CardSuit suit) {
         super("Wells Fargo", value, suit);
     }
 
+    public void playedBy(Player player){
+        player.drawToHand();
+        player.drawToHand();
+        player.drawToHand();
+    }
 }
