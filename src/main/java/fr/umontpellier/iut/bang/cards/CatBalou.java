6a36e520ae11440b16873e9cc574cf203570f8b9
Gatien Depeyre <gatien.depeyre@etu.umontpellier.fr>
2021-04-24 12:30:01 +0200

    Correction catBalou

--- src/main/java/fr/umontpellier/iut/bang/cards/CatBalou.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/CatBalou.java
@@ -46,17 +46,47 @@ public class CatBalou extends OrangeCard {
         Card choisie=null;
         if(choix.equals(target.getName()))
             choisie=target.removeRandomCardFromHand();
-        else{
-            if(target.getWeapon()!=null && target.getWeapon().getName().equals(choix))
-                choisie=target.getWeapon();
+        else {
+            if (target.getWeapon() != null && target.getWeapon().getName().equals(choix)){
+                choisie = target.getWeapon();
+                target.setWeapon(null);
+            }
             else
                 for(BlueCard c : target.getInPlay())
                     if(c.getName().equals(choix)) {
                         choisie = c;
                         target.removeFromInPlay(c);
+                        break;
                     }
         }
         player.getGame().addToDiscard(choisie);
         player.getGame().addToDiscard(this);
+    }
+
+    /**
+     * Cette fonction est juste un test pour refactoriser la fonction playedBy,
+     * si ca ne marche pas je la supprimerais (Gatien)
+     */
+    public void testFOnctionPlusOptiPlayedBy(Player player){
+        //Toutes les cartes exposées
+        ArrayList<String> possibilites = new ArrayList<>();
+
+        //Pour chaque autre joueur, on rajoute son inPlay, son arme et son nom
+        for(Player p : player.getOtherPlayers()){
+            //inPlay
+            for (BlueCard bc : p.getInPlay())
+                possibilites.add(bc.getName());
+            //Weapon
+            if(p.getWeapon()!=null)
+                possibilites.add(p.getWeapon().getName());
+            //name
+            possibilites.add(p.getName());
+        }
+
+        String choix = player.choose("Cliquez sur la carte à prendre ou sur le joueur pour prendre dans la main",
+                new ArrayList<>(possibilites), true, false);
+
+
+
     }
 }
