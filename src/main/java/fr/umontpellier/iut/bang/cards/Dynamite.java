b5741762672218fa601e8376c6d4832eb5cec4c5
Gatien Depeyre <gatien.depeyre@etu.umontpellier.fr>
2021-04-24 14:00:12 +0200

    Dynamites qui explose au début du tour et passe au suivant sinon

--- src/main/java/fr/umontpellier/iut/bang/cards/Dynamite.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Dynamite.java
@@ -3,21 +3,40 @@ package fr.umontpellier.iut.bang.cards;
 import fr.umontpellier.iut.bang.Player;
 
 public class Dynamite extends BlueCard {
+    private boolean active;
+
 
     public Dynamite(int value, CardSuit suit) {
         super("Dynamite", value, suit);
+        active = false;
     }
 
     @Override
     public void playedBy(Player player) {
-        player.addToInPlay(this);
+        if(!active) {
+            player.addToInPlay(this);
+            active=true;
+        }
+        else{
+            tryToExplodeOn(player);
+        }
+    }
+
+    public void tryToExplodeOn(Player player){
         Card pioche = player.randomDraw();
         if(pioche.getValue() >= 2 && pioche.getValue() <=9 && pioche.getSuit().toJSON().equals("S")){
             player.decrementHealth(3, null);
             player.discardFromInPlay(player.getCardInPlay("Dynamite"));
         }
         else{
-            player.getOtherPlayers();
+            //On passe la dynamite au suivant
+            int index = player.getGame().getPlayers().indexOf(player);
+            index += 1;
+            index %= player.getGame().getPlayers().size();
+            Player target = player.getGame().getPlayers().get(index);
+
+            player.removeFromInPlay(this);
+            target.addToInPlay(this);
         }
     }
 
