8bf1512f5382323db0213098fe64f8a8da28518b
BastienGavioli <bastien.gavioli34@gmail.com>
2021-04-28 14:22:37 +0200

    Simplification de la classe Beer, suppression de la fonction heal()

--- src/main/java/fr/umontpellier/iut/bang/cards/Beer.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Beer.java
@@ -4,23 +4,14 @@ import fr.umontpellier.iut.bang.Player;
 
 public class Beer extends OrangeCard {
 
-    private boolean healEffectActive;
-
     public Beer(int value, CardSuit suit) {
         super("Beer", value, suit);
-        healEffectActive = false;
-    }
-
-    public void heal(Player player) {
-        if(healEffectActive)
-            player.incrementHealth(1);
     }
 
     @Override
     public void playedBy(Player player) {
         super.playedBy(player);
         if(player.getGame().getPlayers().size()>2)
-            healEffectActive = true;
-        this.heal(player);
+            player.incrementHealth(1);
     }
 }
