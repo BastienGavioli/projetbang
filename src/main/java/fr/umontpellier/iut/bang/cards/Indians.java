20698e2d76e53db7cb7f84206a49db9427f20706
Mélanie Fressard <fressardmelanie@gmail.com>
2021-04-28 13:20:23 +0200

    essai indians

--- src/main/java/fr/umontpellier/iut/bang/cards/Indians.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Indians.java
@@ -1,9 +1,34 @@
 package fr.umontpellier.iut.bang.cards;
 
+import fr.umontpellier.iut.bang.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Indians extends OrangeCard {
 
     public Indians(int value, CardSuit suit) {
         super("Indians!", value, suit);
     }
 
+    @Override
+    public void playedBy(Player player) {
+        super.playedBy(player);
+        //fait un effet bang sur les autres joueurs
+        for(Player p : player.getOtherPlayers()){
+            Bang.bangEffect(p, player);
+            //On enlève toutes les cartes qui ne sont pas des bang de la main
+            List<Card> bangCards = new ArrayList<>(p.getHand());
+            bangCards.removeIf(c -> !c.getName().equals("Bang!"));
+
+            //Le joueur choisit s'il veut jouer un Bang! ou non
+            Card bang = p.chooseCard("Jouez une carte Bang! ou passez",
+                    bangCards, false, true);
+            if (bang == null) {
+                p.decrementHealth(1, player);
+            } else {
+                p.playFromHand(bang);
+            }
+        }
+    }
 }
