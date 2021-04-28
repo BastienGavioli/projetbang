5cf041b01393cf3dae3e4fa3794032e51a09980e
BastienGavioli <bastien.gavioli34@gmail.com>
2021-04-28 14:12:47 +0200

    Mise à jour classe Beer

--- src/main/java/fr/umontpellier/iut/bang/cards/Beer.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Beer.java
@@ -4,17 +4,23 @@ import fr.umontpellier.iut.bang.Player;
 
 public class Beer extends OrangeCard {
 
+    private boolean healEffectActive;
+
     public Beer(int value, CardSuit suit) {
         super("Beer", value, suit);
+        healEffectActive = false;
     }
 
     public void heal(Player player) {
-        player.incrementHealth(1);
+        if(healEffectActive)
+            player.incrementHealth(1);
     }
 
     @Override
     public void playedBy(Player player) {
         super.playedBy(player);
+        if(player.getGame().getPlayers().size()>2)
+            healEffectActive = true;
         this.heal(player);
     }
 }
