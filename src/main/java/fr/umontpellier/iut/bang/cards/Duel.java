f0a38269b8928afdf4fb8d1f9c83afabd4a37b48
Mélanie Fressard <fressardmelanie@gmail.com>
2021-04-28 15:49:28 +0200

    ajout duel

--- src/main/java/fr/umontpellier/iut/bang/cards/Duel.java
+++ src/main/java/fr/umontpellier/iut/bang/cards/Duel.java
@@ -1,9 +1,58 @@
 package fr.umontpellier.iut.bang.cards;
 
+import fr.umontpellier.iut.bang.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Duel extends OrangeCard {
 
     public Duel(int value, CardSuit suit) {
         super("Duel", value, suit);
     }
 
+    @Override
+    public void playedBy(Player player) {
+        super.playedBy(player);
+        Player target;
+        target = player.choosePlayer("Qui voulez vous défier ?", player.getOtherPlayers(), false);
+
+        //On enlève toutes les cartes qui ne sont pas des bang de la main de la cible
+        List<Card> bangCards = new ArrayList<>(target.getHand());
+        bangCards.removeIf(c -> !c.getName().equals("Bang!"));
+
+        //On fait pareil pour le joueur
+        List<Card> bangCardsP = new ArrayList<>(player.getHand());
+        bangCardsP.removeIf(c -> !c.getName().equals("Bang!"));
+
+        //La cible choisit si elle veut jouer le 1er Bang! ou non
+        Card bang = target.chooseCard("Jouez une carte Bang! ou passez",
+                bangCards, false, true);
+
+        if (bang == null) { //si elle choisit de ne pas lancer le duel elle perd un pts de vie
+            target.decrementHealth(1, player);
+        }else { //sinon le duel est lance
+            //Le joueur choisit s'il veut jouer un 1er Bang! ou non
+            Card bangP = player.chooseCard("Jouez une carte Bang! ou passez",
+                    bangCardsP, false, true);
+
+            //tant que les 2 peuvent/veulent jouer des bang
+            while (bang != null && bangP != null) {
+                //La cible choisit si elle veut jouer un Bang! ou non
+                bang = target.chooseCard("Jouez une carte Bang! ou passez",
+                        bangCards, false, true);
+
+                //Le joueur choisit s'il veut jouer un Bang! ou non
+                bangP = player.chooseCard("Jouez une carte Bang! ou passez",
+                        bangCardsP, false, true);
+            }
+
+            //a la sortie de la boucle on verifie qui n'a plus de bang / qui a refuser de jouer un bang
+            if (bang == null) {
+                target.decrementHealth(1, player);
+            } else {
+                player.decrementHealth(1, target);
+            }
+        }
+    }
 }
