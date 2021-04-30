f653834105b6f5bb1e6d9b2b4c01928fc91bfe37
Mélanie Fressard <fressardmelanie@gmail.com>
2021-04-30 16:27:34 +0200

    ajout de kit et pedro (kit ne fonctionne pas correctement)

--- src/main/java/fr/umontpellier/iut/bang/characters/KitCarlson.java
+++ src/main/java/fr/umontpellier/iut/bang/characters/KitCarlson.java
@@ -1,9 +1,40 @@
 package fr.umontpellier.iut.bang.characters;
 
+import fr.umontpellier.iut.bang.Player;
+import fr.umontpellier.iut.bang.cards.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class KitCarlson extends BangCharacter {
 
     public KitCarlson() {
         super("Kit Carlson", 4);
     }
 
+    /*Durant la phase 1 de son tour, il regarde les trois premières cartes de la pioche,
+    en choisit 2 qu’il garde et repose la troisième sur la pioche, face cachée.
+    Détails : Au début du tour, le joueur doit entrer le nom de la carte qu'il veut replacer sur la pioche
+    (c'est plus simple que de lui faire choisir deux fois les cartes qu'il veut garder).
+    La carte est replacée sur le dessus de la pioche et les deux autres cartes tirées sont placées dans sa main.
+    Remarques :
+    Vous pouvez passer true comme valeur à l'argument showButtons de la méthode Player.chooseCard()
+    pour afficher à l'écran les trois cartes qui ont été piochées (dans l'interface graphique et en mode console)*/
+
+    @Override
+    public void onStartTurn(Player player) {
+        List<Card> possibilites = new ArrayList<>();
+        possibilites.add(player.drawCard());
+        possibilites.add(player.drawCard());
+        possibilites.add(player.drawCard());
+        Card reposer = player.chooseCard("Quelle carte voulez vous reposer ?", possibilites, true, false);
+
+        player.getGame().addToDiscard(reposer);
+        possibilites.remove(reposer);
+
+        for(int i = 0; i<=1; i++)
+            player.addToHand(possibilites.get(i));
+
+
+    }
 }
--- src/main/java/fr/umontpellier/iut/bang/characters/PedroRamirez.java
+++ src/main/java/fr/umontpellier/iut/bang/characters/PedroRamirez.java
@@ -1,9 +1,34 @@
 package fr.umontpellier.iut.bang.characters;
 
+import fr.umontpellier.iut.bang.Player;
+import fr.umontpellier.iut.bang.cards.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PedroRamirez extends BangCharacter {
 
     public PedroRamirez() {
         super("Pedro Ramirez", 4);
     }
 
+    /*Durant la phase 1 de son tour, il peut choisir de piocher la première carte de la défausse
+    au lieu de la prendre dans la pioche. Il pioche sa seconde carte normalement, dans la pioche.
+    Détails : Au début de son tour et si la défausse n'est pas vide le joueur peut entrer le nom
+    de la carte qui se trouve sur le dessus de la défausse pour la piocher en main, ou passer pour
+    prendre sa première carte dans la pioche. La seconde carte est automatiquement prise dans la pioche.
+    Remarques :
+    Si la défausse est vide, le joueur pioche automatiquement ses deux cartes dans la pioche*/
+
+    @Override
+    public void onStartTurn(Player player) {
+        List<Card> possibilite = new ArrayList<>();
+        possibilite.add(player.getGame().getTopOfDiscardPile());
+        Card choix = player.chooseCard("Voulez vous prendre la carte de la défausse ?", possibilite, true, true);
+        if (choix != null)
+            player.addToHand(possibilite.get(0));
+        else
+            player.drawToHand();
+        player.drawToHand();
+    }
 }
